3a196ac55f53cc7151160e878d40b76cde7e690c
Armand Maree <aamaree@gmail.com>
2018-05-27 20:24:28 +0200

    Initial Website
    Added the initial version of  Ember.js and also the first tests for Travis. This is mostly a test commit to make sure everything is working as expected.

---
+++ frontend/website/app/routes/blogs.js
@@ -0,0 +1,17 @@
+import Route from '@ember/routing/route';
+
+export default Route.extend({
+	model() {
+		return [{
+			"title": "C++ Pointers",
+			"date": "27/05/2018 15:00",
+			"short": "Lorem ipsum dolor sit amet, tibique ancillae pericula eos id. Ad quo quis repudiandae, mei ne eros apeirian consequat. Est legere minimum eu, eu nam vocent audiam deseruisse. Dicta dolorum omittantur vim cu. Insolens indoctum ne vis, usu philosophia delicatissimi ad, harum delectus similique pro et.",
+			"image": "logo.png"
+		}, {
+			"title": "MINIX 3",
+			"date": "26/05/2018 11:54",
+			"short": "Lorem ipsum dolor sit amet, tibique ancillae pericula eos id. Ad quo quis repudiandae, mei ne eros apeirian consequat. Est legere minimum eu, eu nam vocent audiam deseruisse. Dicta dolorum omittantur vim cu. Insolens indoctum ne vis, usu philosophia delicatissimi ad, harum delectus similique pro et.",
+			"image": "logo.png"
+		}];
+	}
+});
