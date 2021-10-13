ab461d031531527105d0b1a267f42574d7e98590
Armand Maree <aamaree@gmail.com>
2021-10-13 11:00:19 -0400

    Added suupport for dotenv in sequelize migrations

---
+++ migration/config/config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  local: {
+    username: 'dbmasteruser',
+    password: 'r6x~qm_*z(.ymyK|xA~p4p[{~`nBGx88',
+    database: 'dbaquariummanager_test',
+    host: 'ls-112fc322afec2f70b4b4d031fc0b3265efba2104.crhvt3cwpory.us-east-1.rds.amazonaws.com',
+    dialect: 'mysql',
+    logging: true,
+  },
+  development: {
+    username: 'dbmasteruser',
+    password: 'r6x~qm_*z(.ymyK|xA~p4p[{~`nBGx88',
+    database: 'dbaquariummanager_test',
+    host: 'ls-112fc322afec2f70b4b4d031fc0b3265efba2104.crhvt3cwpory.us-east-1.rds.amazonaws.com',
+    dialect: 'mysql',
+    logging: true,
+  },
+  test: {
+    username: 'dbmasteruser',
+    password: 'r6x~qm_*z(.ymyK|xA~p4p[{~`nBGx88',
+    database: 'dbaquariummanager_test',
+    host: 'ls-112fc322afec2f70b4b4d031fc0b3265efba2104.crhvt3cwpory.us-east-1.rds.amazonaws.com',
+    dialect: 'mysql',
+  },
+  production: {
+    username: 'dbmasteruser',
+    password: 'r6x~qm_*z(.ymyK|xA~p4p[{~`nBGx88',
+    database: 'dbaquariummanager',
+    host: 'ls-112fc322afec2f70b4b4d031fc0b3265efba2104.crhvt3cwpory.us-east-1.rds.amazonaws.com',
+    dialect: 'mysql',
+  },
+};
