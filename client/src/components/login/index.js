d86be9ba06fb32de04770a235555b2e84033e3b7
Armand Maree <aamaree@gmail.com>
2021-12-10 21:25:53 -0500

    Changed file name to lowercase

--- client/src/components/login/index.js
+++ client/src/components/login/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Login.css';
+import './login.css';
 import { Button } from 'react-bootstrap'
 import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.min.css'
